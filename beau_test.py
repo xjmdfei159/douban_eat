7032fe416d2a853ebf4fc073082ab1ae0ffc0f13
kabi <xjmdfei159@gmail.com>
2016-07-19 23:57:51 +0800

    针对一个网页解析title website pic

--- beau_test.py
+++ beau_test.py
@@ -13,11 +13,17 @@ from bs4 import BeautifulSoup
 import urllib
 
 def main():
-	html_doc = "https://www.douban.com/group/"
+	html_doc = "https://www.douban.com/subject/all?cat_id=1000"
 	page = urllib.urlopen(html_doc)
 	#html = page.read()
 	soup = BeautifulSoup(page,'html.parser')
-	print(soup.title)
+	test = soup.findAll('div',{'class':"pic"})
+	for i in range(0,len(test)):
+		test_title = test[i].a['title']
+		test_web = test[i].a['href']
+		test_pic = test[i].img['src']
+		print(test_title + ' web:' + test_web +	' pic:' + test_pic)
+
 
 
 if __name__ == '__main__':
