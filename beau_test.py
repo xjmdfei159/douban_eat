95c68ecdc765e3936c71aeb1946db5ed2ed6f23d
kabi <xjmdfei159@gmail.com>
2016-07-20 00:23:57 +0800

    add like num

--- beau_test.py
+++ beau_test.py
@@ -17,13 +17,15 @@ def main():
 	page = urllib.urlopen(html_doc)
 	#html = page.read()
 	soup = BeautifulSoup(page,'html.parser')
-	test = soup.findAll('div',{'class':"pic"})
+	test = soup.findAll('li',{'class':"thing-item"})
+	test_li = soup.findAll('span',{'class':"favs"})
+	print(len(test))
 	for i in range(0,len(test)):
 		test_title = test[i].a['title']
 		test_web = test[i].a['href']
 		test_pic = test[i].img['src']
-		print(test_title + ' web:' + test_web +	' pic:' + test_pic)
-
+		test_like = test_li[i].get_text()
+		print(test_title + ' web:' + test_web +	' pic:' + test_pic + ' like:' +test_like)
 
 
 if __name__ == '__main__':
