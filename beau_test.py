96fe9e75cdcd2d38d3ec51b365ad16aef829aa9b
kabi <xjmdfei159@gmail.com>
2016-07-20 00:30:06 +0800

    爬了20个网页的~~~

--- beau_test.py
+++ beau_test.py
@@ -13,19 +13,20 @@ from bs4 import BeautifulSoup
 import urllib
 
 def main():
-	html_doc = "https://www.douban.com/subject/all?cat_id=1000"
-	page = urllib.urlopen(html_doc)
-	#html = page.read()
-	soup = BeautifulSoup(page,'html.parser')
-	test = soup.findAll('li',{'class':"thing-item"})
-	test_li = soup.findAll('span',{'class':"favs"})
-	print(len(test))
-	for i in range(0,len(test)):
-		test_title = test[i].a['title']
-		test_web = test[i].a['href']
-		test_pic = test[i].img['src']
-		test_like = test_li[i].get_text()
-		print(test_title + ' web:' + test_web +	' pic:' + test_pic + ' like:' +test_like)
+	for i in range(0,20):
+		html_doc = 'https://www.douban.com/subject/all?cat_id=1000&start=' + str(20*i)
+		page = urllib.urlopen(html_doc)
+		#html = page.read()
+		soup = BeautifulSoup(page,'html.parser')
+		test = soup.findAll('li',{'class':"thing-item"})
+		test_li = soup.findAll('span',{'class':"favs"})
+		print(len(test))
+		for i in range(0,len(test)):
+			test_title = test[i].a['title']
+			test_web = test[i].a['href']
+			test_pic = test[i].img['src']
+			test_like = test_li[i].get_text()
+			print(test_title + ' web:' + test_web +	' pic:' + test_pic + ' like:' +test_like)
 
 
 if __name__ == '__main__':
