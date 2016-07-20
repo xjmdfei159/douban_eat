09d26370b4d37e6f64ab13f73bdce6b4ca7729bd
xjmdfei159 <xjmdfei159@gmail.com>
2016-07-20 13:03:41 +0800

    改为while先

--- beau_test.py
+++ beau_test.py
@@ -12,21 +12,28 @@
 from bs4 import BeautifulSoup
 import urllib
 
-def main():
-	for i in range(0,30):
-		html_doc = 'https://www.douban.com/subject/all?cat_id=1000&start=' + str(20*i)
+def eat_spider():
+	page_num = 0
+	while(1):
+
+
+		html_doc = 'https://www.douban.com/subject/all?cat_id=1000&start=' + str(20*page_num)
 		page = urllib.urlopen(html_doc)
 		#html = page.read()
 		soup = BeautifulSoup(page,'html.parser')
 		test = soup.findAll('li',{'class':"thing-item"})
 		test_li = soup.findAll('span',{'class':"favs"})
-		print(len(test))
 		for i in range(0,len(test)):
 			test_title = test[i].a['title']
 			test_web = test[i].a['href']
 			test_pic = test[i].img['src']
 			test_like = test_li[i].get_text()
 			print(test_title + ' web:' + test_web +	' pic:' + test_pic + ' like:' +test_like)
+		page_num += 1
+	return 2
+
+def main():
+	eat_spider()
 
 
 if __name__ == '__main__':
