995716e27091e834b3f7d68cf3d9e6f6bf306fe9
xjmdfei159 <xjmdfei159@gmail.com>
2016-07-28 20:21:44 +0800

    找出最低的敲打次数先

--- sword_simu.py
+++ sword_simu.py
@@ -1,8 +1,5 @@
 __author__ = 'hzfanyaling'
-
-#目的：
-#每个县平均获得1 然后每个属性都从0-3里面随机数字 总和如果超过15 则随机从最大的属性中-1 直至15
-#每次敲打要在一定的数量级中随机 然后敲第十次会基础值增加 然后整体会随着敲击的次数而增加
+#!/usr/bin/env python
 
 
 
@@ -11,30 +8,32 @@ import random
 sword = "1:9,30|2:9,30|3:99,300|5:4,15|9:2,9"
 sword_split = sword.split('|')
 sword_prob = []
-prob1_num = 1
-prob2_num = 1
-prob3_num = 1
-prob4_num = 1
-prob5_num = 1
-sword_prob_num = [prob1_num,prob2_num,prob3_num,prob4_num,prob5_num]
-sword_beat_max_num = 4
-sword_beat_num = 15
+base_num = 1
+probA_num = 1+random.randint(0,3)
+probB_num = 1+random.randint(0,3)
+probC_num = 1+random.randint(0,3)
+probD_num = 1+random.randint(0,3)
+probE_num = 1+random.randint(0,3)
+sword_prob_num = [probA_num,probB_num,probC_num,probD_num,probE_num]
+sword_simu_num = sum(sword_prob_num)
+sword_extra_num = 0
+sword_max_num = 15
+print(sword_prob_num)
+print(sword_prob_num.index(min(sword_prob_num)))
+def simu_num():
+	if(sword_simu_num==sword_max_num):
+		print(sword_prob_num)
+	elif (sword_simu_num<15):
+		sword_extra_num = sword_max_num - sword_simu_num
 
-for i in range(5):
-	sword_prob_sp = sword_split[i]
-	sword_prob.append(sword_prob_sp)
+		print('sword_simu_num' + str(sword_simu_num))
+	else:
 
-for i in range(1):
-	num = random.randint(0,sword_beat_num-5)
-	print(num)
-	if(num>sword_beat_max_num-1):
-		num = sword_beat_max_num - 1
-		print(num)
+		sword_extra_num = sword_simu_num - sword_max_num
 
-
+		print(sword_extra_num)
 def main():
-
- print('haha')
+	simu_num()
 
 
 
