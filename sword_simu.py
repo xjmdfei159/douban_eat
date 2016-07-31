acf2698529a6bc0d911b6345df188e23b3cabd3b
kabi <xjmdfei159@gmail.com>
2016-07-31 23:56:03 +0800

    先mark下还有很多问题啊。

--- sword_simu.py
+++ sword_simu.py
@@ -1,19 +1,15 @@
 __author__ = 'hzfanyaling'
 #!/usr/bin/env python
 
-
-
 import random
-
 sword = "1:9,30|2:9,30|3:99,300|5:4,15|9:2,9"
 sword_split = sword.split('|')
+
 type_weights_map = {}
-real_weights_map = {}
+result_weights_map = {}
 for data in sword_split:
 	attr_type,weight_type = data.split(':')
-	type_weights_map[attr_type]=weight_type.split(',')
-	#print(type_weights_map)
-
+	type_weights_map[attr_type] = weight_type.split(',')
 
 sword_attr_time = []    #sword prob list
 base_num = 1
@@ -34,28 +30,42 @@ sword_baodi_coe = 0.3  #baodi
 sword_result = sword_fif_num*sword_fif_coe + sword_ten_num*sword_ten_coe
 
 def attr_real():
-	for i in range(2):
-		type_weights_map['1'][i] = float(type_weights_map['1'][i])*(sword_result+sword_min_coe)
-		print(type_weights_map['1'])
-
+	for k,v in type_weights_map.items():
+		for i in range(2):
+			type_weights_map[k][i] = float(type_weights_map[k][i])*(sword_result + sword_min_coe)
+			#print(type_weights_map)
 
 def sword_attr_func():
 	attr_real()
 	for i in range(5):
 		sword_attr_num = 1 + random.randint(0,3)
 		sword_attr_time.append(sword_attr_num)
-		print(sword_attr_time)
+		#print(sword_attr_time)
 	sword_simu_num = sum(sword_attr_time)
 	# compare sword_simu_num vs sword_max_num
 	if (sword_simu_num == sword_max_num):
 		sword_attr_cal()
 
 def sword_attr_cal():
-	print(3)
+
+	probA = []
+	test = []
+	for k in type_weights_map.iterkeys():
+		haha = type_weights_map[k]
+		test.append(haha)
+
+	print(test)
+	print(sword_attr_time)
+	for i in range(5):
+		for j in range(sword_attr_time[i]):
+			kabi = round(random.uniform(test[i][0],test[i][1]),2)
+			probA.append(kabi)
+	print(probA)
+
 
 def main():
 	sword_attr_func()
-
+	sword_attr_cal()
 
 
 if __name__ == '__main__':
